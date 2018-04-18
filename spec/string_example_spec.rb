270e4fca3b0f5237b170326e22e15a597ec2ea70
Ashley Margallo <amarie921@yahoo.com>
2018-04-18 10:55:56 -0600

    string helpers

--- spec/string_example_spec.rb
+++ spec/string_example_spec.rb
@@ -1,9 +1,21 @@
 RSpec.describe StringExample do
+  subject { StringExample }
+  let(:str) { 'MyString' }
+
   it "has a version number" do
     expect(StringExample::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "reverses a string" do
+    actual = subject.reversify(str)
+    expected = 'gnirtS yM'
+    expect(actual).to eq(expected)
   end
+
+  it 'adds spaces' do
+    actual = subject.spacify(str, 1)
+    expeced = "M y   S t r i n g"
+    expect(actual).to eq(expeced)
+  end
+  
 end
